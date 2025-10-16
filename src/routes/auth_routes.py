12f891bf0610d76c8aa625954d9b90924963fbc1
Sergio Mejia <sergio12mg25@gmail.com>
2025-10-16 14:05:15 -0500

    Uso del modelo Token en el endpoint de autenticación

--- src/routes/auth_routes.py
+++ src/routes/auth_routes.py
@@ -1,22 +1,19 @@
 from datetime import timedelta
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
-from sqlmodel import Session, select
+from sqlmodel import Session
 
 from src.utils.database import get_session
-from src.models.BaseModels import Usuario
+from src.models.Token import Token
 from src.services.auth_service import authenticate_user
-from src.utils.auth import (
-    create_access_token, hash_password, verify_password,
-    ACCESS_TOKEN_EXPIRE_MINUTES
-)
+from src.utils.auth import create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
 
 # Objeto para definir endpoints
 router = APIRouter(prefix="/api/v1/auth", tags=["auth"])
 
 # Endpoint para iniciar sesión
-@router.post("/token")
-def login(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(get_session)):
+@router.post("/token", response_model=Token)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(get_session)) -> Token:
     # 1. Obtener el nombre de usuario y contraseña del formulario
     user = authenticate_user(session, form_data.username, form_data.password)
 
@@ -24,15 +21,15 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = D
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Nombre de usuario o contraseña incorrectos",
+            detail="Correo o contraseña incorrectos",
             headers={"WWW-Authenticate": "Bearer"},
         )
     
     # 3. Generar el token
-    access_token = create_access_token(data={"sub": user.nombre}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    access_token = create_access_token(data={"sub": user.correo}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
 
-    return {
-        "access_token": access_token,
-        "token_type": "bearer",
-        "expires_in": ACCESS_TOKEN_EXPIRE_MINUTES * 60
-    }
+    return Token(
+        access_token=access_token,
+        token_type="bearer",
+        expires_in=ACCESS_TOKEN_EXPIRE_MINUTES * 60
+    )
